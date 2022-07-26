3344ab76bc6f381fa94bbb6bdfbe449e83357ba5
web.ljg <web.ljg@gmail.com>
2022-07-26 15:36:05 +0900

    feat: firebase auth 활용 로그인 구현

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,15 +1,29 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
 import Auth from 'routes/Auth';
 import Home from 'routes/Home';
+import Navigation from 'components/Navigation';
+import Profile from 'routes/Profile';
 
 const AppRouter = ({ isLoggedIn }) => {
   return (
     <Router>
+      {isLoggedIn && <Navigation />}
       <Routes>
         {isLoggedIn ? (
-          <Route path="/" element={<Home />}></Route>
+          <>
+            <Route path="/" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
+          </>
         ) : (
-          <Route path="/" element={<Auth />}></Route>
+          <>
+            <Route path="/" element={<Auth />} />
+            <Route path="*" element={<Navigate replace to="/" />} />
+          </>
         )}
       </Routes>
     </Router>
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,3 +1,19 @@
-const Profile = () => <span>Profile</span>;
+import { authService } from 'fbase';
+import { useNavigate } from 'react-router-dom';
+
+const Profile = () => {
+  const navigate = useNavigate();
+  const onLogOut = () => {
+    authService.signOut();
+    navigate('/');
+  };
+  return (
+    <>
+      <button type="button" onClick={onLogOut}>
+        Log Out
+      </button>
+    </>
+  );
+};
 
 export default Profile;
