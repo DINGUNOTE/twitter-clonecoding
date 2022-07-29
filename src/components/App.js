83df0e112b6a4b267262096e2fb1e8e5bad9d6ae
web.ljg <web.ljg@gmail.com>
2022-07-29 16:07:28 +0900

    feat: Profile 수정 기능 구현, 로그아웃 수정

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import AppRouter from 'components/Router';
 import { authService } from 'fbase';
+import { updateProfile } from 'firebase/auth';
 
 const App = () => {
   const [init, setInit] = useState(false);
@@ -9,15 +10,42 @@ const App = () => {
   useEffect(() => {
     authService.onAuthStateChanged(user => {
       if (user) {
-        setUserObj(user);
+        if (user.displayName === null) {
+          updateProfile(user, { displayName: 'Anonymous' });
+        }
+
+        setUserObj({
+          displayName: user.displayName,
+          uid: user.uid,
+          updateProfile: () =>
+            updateProfile(user, { displayName: user.displayName }),
+        });
+      } else {
+        setUserObj(null);
       }
       setInit(true);
     });
   }, []);
+
+  const refreshUser = () => {
+    const user = authService.currentUser;
+
+    setUserObj({
+      displayName: user.displayName,
+      uid: user.uid,
+      updateProfile: () =>
+        updateProfile(user, { displayName: user.displayName }),
+    });
+  };
+
   return (
     <>
       {init ? (
-        <AppRouter isLoggedIn={Boolean(userObj)} userObj={userObj} />
+        <AppRouter
+          isLoggedIn={Boolean(userObj)}
+          userObj={userObj}
+          refreshUser={refreshUser}
+        />
       ) : (
         'Initializing...'
       )}
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,14 +1,68 @@
-import { authService } from 'fbase';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { authService, dbService } from 'fbase';
+import { updateProfile } from 'firebase/auth';
+import { collection, getDocs, orderBy, query, where } from 'firebase/firestore';
 
-const Profile = () => {
+const Profile = ({ userObj, refreshUser }) => {
   const navigate = useNavigate();
+  const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
+
+  const getMyNweets = async () => {
+    // dbService의 컬렉션 중 "nweets" Docs에서 userObj의 uid와 동일한 creatorID를 가진 모든 문서를 내림차순으로 가져오는 쿼리(요청) 생성
+    const q = query(
+      collection(dbService, 'nweets'),
+      where('creatorId', '==', userObj.uid),
+      orderBy('createdAt', 'desc'),
+    );
+
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach(doc => {
+      console.log(doc.id, '=>', doc.data());
+    });
+  };
+
+  useEffect(() => {
+    getMyNweets();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const onChange = e => {
+    const {
+      target: { value },
+    } = e;
+    setNewDisplayName(value);
+  };
+
+  const onSubmit = async e => {
+    e.preventDefault();
+    if (userObj.displayName !== newDisplayName) {
+      await updateProfile(authService.currentUser, {
+        displayName: newDisplayName,
+      });
+      refreshUser();
+    } else {
+      return;
+    }
+  };
+
   const onLogOut = () => {
     authService.signOut();
+    // authService.currentUser.uid;
     navigate('/');
   };
+
   return (
     <>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          onChange={onChange}
+          placeholder="Display name"
+          value={newDisplayName}
+        />
+        <input type="submit" value="Update Profile" />
+      </form>
       <button type="button" onClick={onLogOut}>
         Log Out
       </button>
